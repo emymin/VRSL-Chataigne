cde97ba9ba99b48491bb785b00465ff33a353866
emymin <emmyminmon@gmail.com>
2023-01-04 19:28:11 +0100

    Reworked to load external definitions file

--- vrsl-lights.js
+++ vrsl-lights.js
@@ -1,78 +1,8 @@
 var fixtures;
-
-var descriptions = {
-    "pantilt":"Pan and Tilt of the light",
-    "finepantilt":"Finer Pan and Tilt of the light",
-    "conewidth":"Spotlight radius",
-    "intensity":"Intensity of the light",
-    "strobe":"Strobe effect (0-009 is off, 010-255 is slow to fast)",
-    "color":"Color of the light",
-    "goboselect":"GOBO Selection (0 is no gobo, 1-6 are various gobos)",
-    "gobospeed":"Speed at which the GOBO spins",
-    "moverspeed":"Mover Speed",
-    "length":"Length of the beams",
-    "width":"Width of the beams",
-    "flatness":"Flatness of the beams",
-    "beamcount":"Amount of beams",
-    "beamthickness":"Thickness of the beams",
-    "spinspeed":"Speed at which the beams spin"
-};
-
-var definition = {
-    "spotlight":{
-        "pantilt":{"mapping":[1,3]},
-        "finepantilt":{"mapping":[2,4]},
-        "conewidth":{"mapping":[5]},
-        "intensity":{"mapping":[6]},
-        "strobe":{"mapping":[7]},
-        "color":{"mapping":[8,9,10]},
-        "gobospeed":{"mapping":[11]},
-        "goboselect":{"mapping":[12]},
-        "moverspeed":{"mapping":[13]}
-    },
-    "washlight":{
-        "pantilt":{"mapping":[1,3]},
-        "finepantilt":{"mapping":[2,4]},
-        "conewidth":{"mapping":[5]},
-        "intensity":{"mapping":[6]},
-        "strobe":{"mapping":[7]},
-        "color":{"mapping":[8,9,10]},
-        "gobospeed":{"mapping":[13]}
-    },
-    "laser":{
-        "pantilt":{"mapping":[1,2]},
-        "length":{"mapping":[3]},
-        "width":{"mapping":[4]},
-        "flatness":{"mapping":[5]},
-        "beamcount":{"mapping":[6]},
-        "spinspeed":{"mapping":[7]},
-        "color":{"mapping":[8,9,10]},
-        "intensity":{"mapping":[11]},
-        "beamthickness":{"mapping":[12]},
-        "moverspeed":{"mapping":[13]}
-    },
-    "discoball":{
-        "intensity":{"mapping":[1]}
-    },
-    "flasher":{
-        "intensity":{"mapping":[1]}
-    },
-    "blinder":{
-        "intensity":{"mapping":[1]},
-        "color":{"mapping":[2,3,4]},
-        "strobe":{"mapping":[5]}
-    },
-    "barlight":{
-        "intensity":{"mapping":[1]},
-        "color":{"mapping":[2,3,4]},
-        "strobe":{"mapping":[5]}
-    },
-    "parlight":{
-        "intensity":{"mapping":[1]},
-        "color":{"mapping":[2,3,4]},
-        "strobe":{"mapping":[5]}
-    }
-};
+var groups;
+var channel_info;
+var fixture_info;
+var parameterPath;
 
 function init(){
     script.log("VRSL-LIGHTS initialized");
@@ -85,41 +15,53 @@ function init(){
 
 function loadFixtures(){
     script.log("Loading fixtures...");
-    fixtures = util.readFile(local.parameters.setup.fixtures.get(),true);
-    var fixtureKeys = util.getObjectProperties(fixtures); 
 
+    var definitions = util.readFile(local.parameters.setup.definitions.get(),true);
+    channel_info = definitions["channel_info"];
+    fixture_info = definitions["fixture_info"];
+
+    var configuration = util.readFile(local.parameters.setup.fixtures.get(),true);
+    fixtures = configuration["fixtures"];
+    groups = configuration["groups"];
+    
     parameterPath = local.getChild("Parameters");
     var lightsContainer = parameterPath.addContainer("Lights");
     lightsContainer.setCollapsed(true);
     var groupsContainer = parameterPath.addContainer("Groups");
     groupsContainer.setCollapsed(true);
-
+    
+    var fixtureKeys = util.getObjectProperties(fixtures); 
     for(var i=0;i<fixtureKeys.length;i++){
         var key = fixtureKeys[i];
-        var isGroup = (fixtures[key].type=="group");
-        var container = isGroup ? groupsContainer.addContainer(key) : lightsContainer.addContainer(key);
-        createParameters(container,key,fixtures[key].type);
+        var channels = util.getObjectProperties(fixture_info[fixtures[key].type]);
+        createParameters(lightsContainer.addContainer(key),key,channels);
     }
+    var groupKeys = util.getObjectProperties(groups);
+    for(var i=0;i<groupKeys.length;i++){
+        var key = groupKeys[i];
+        createParameters(groupsContainer.addContainer(key),key,groups[key].parameters);
+    }
+    script.log("Loaded "+fixtureKeys.length+" fixtures and "+groupKeys.length+" groups");
 }
-function createParameters(container,name,type){
-    var definitionKeys = util.getObjectProperties(definition[type]);
-    if(type=="group")
-    for(var i=0;i<definitionKeys.length;i++){
-        var channelName = definitionKeys[i];
-        script.log(channelName+": "+descriptions[channelName]);
-        var length = definition[type][channelName].mapping.length;
+function createParameters(container,name,channels){
+    script.log("Creating parameters for "+name);
+    for(var i=0;i<channels.length;i++){
+        var channelName = channels[i];
+        var channelDescription = channel_info[channelName].description;
+        var channelLength = channel_info[channelName].length;
         var p;
-        if(length==1){
-            p = container.addFloatParameter(channelName,descriptions[channelName],0);
-        }else if(length==2){
-            p = container.addPoint2DParameter(channelName,descriptions[channelName],[0,0]);
-        }else if(length==3){
-            p = container.addColorParameter(channelName,descriptions[channelName],[0,0,0,1]);
+        if(channelLength==1){
+            p = container.addFloatParameter(channelName,channelDescription,0);
+        }else if(channelLength==2){
+            p = container.addPoint2DParameter(channelName,channelDescription,[0,0]);
+        }else if(channelLength==3){
+            p = container.addColorParameter(channelName,channelDescription,[0,0,0,1]);
         }else{
             script.log("Unsupported parameter length");
         }
         p.setAttribute("alwaysNotify", true);
     }
+    script.log("Parameters created for "+name);
 }
 function moduleParameterChanged(param){
     script.log("Parameter "+param.name+": "+param);
@@ -132,52 +74,47 @@ function moduleParameterChanged(param){
     var isGroup = param.getParent().getParent().name=="groups";
     var group_or_light_name = param.getParent().name;
     var channel_name = param.name;
-    
+    var channel_length = channel_info[channel_name].length;
     var value = param.get();
+    
     if(!value.length){value = [value];}
-    if(value.length!=definition[channel_name].mapping.length){
-        script.log("Received "+channel_name+" value of length "+value.length+" that doesn't match definition length of "+definition[channel_name].mapping.length);
+    if(value.length!=channel_length){
+        script.log("Received "+channel_name+" value of length "+value.length+" that doesn't match definition length of "+channel_length);
     }
 
-    var lights = [];
-    var members = [];
+    var fixtures_to_modify = [];
+    var members_to_modify = [];
     if(isGroup){
-        members = fixtures[group_or_light_name].members;
-        var memberkeys = util.getObjectProperties(members);
-        for(var i=0;i<memberkeys.length;i++){
-            lights.push(memberkeys[i]);
+        members_to_modify = groups[group_or_light_name].members;
+        var member_names = util.getObjectProperties(members_to_modify);
+        for(var i=0;i<member_names.length;i++){
+            fixtures_to_modify.push(member_names[i]);
         }
     }else{
-        lights.push(group_or_light_name);
+        fixtures_to_modify.push(group_or_light_name);
     }
-    for(var i=0;i<value.length;i++){
-
-        if(channel_name=="pantilt" || channel_name=="finepantilt"){
-            value[i] = (value[i]+1)*0.5;
+    for(var i=0;i<channel_length;i++){
+        var range = channel_info[channel_name].range;
+        if(range){
+            value[i] = (value[i]-range[0]) / (range[1]-range[0]);
         }
-
-        if(channel_name=="goboselect"){
-            value[i] = Math.max(0,Math.min(6,value[i]));
-        }else if(channel_name=="strobe"){
-            value[i] = Math.max(0,Math.min(255,value[i]));
-        }else{
-            value[i] = Math.max(0,Math.min(255,value[i]*255));
-        }
+        value[i] = Math.max(0,Math.min(255,value[i]*255));
     }
 
-    for(var i=0;i<lights.length;i++){
-        var light_name = lights[i];
-        var address = fixtures[light_name].address;
-        script.log("Light name: "+light_name+"\nAddress: "+address+"\nChannel name: "+channel_name);
-        for(var j=0;j<definition[channel_name].mapping.length;j++){
-            var converted = value[j];
-            if( isGroup && (channel_name=="pantilt" || channel_name=="finepantilt") && members[light_name].flip && j==1){
-                converted = 255-converted;
+    for(var i=0;i<fixtures_to_modify.length;i++){
+        var fixture_name = fixtures_to_modify[i];
+        var sector = fixtures[fixture_name].sector;
+        var mapping = fixture_info[fixtures[fixture_name].type][channel_name].mapping;
+        script.log("\nLight name: "+fixture_name+"\nSector: "+sector+"\nChannel name: "+channel_name);
+        for(var j=0;j<channel_length;j++){
+            var final_value = value[j];
+            if( isGroup && (channel_name=="pantilt" || channel_name=="finepantilt") && members_to_modify[fixture_name].flip && j==1){
+                final_value = 255-final_value;
             }
-            var offset = definition[channel_name].mapping[j];
-            var dmx_channel = address * 13 + offset;
-            script.log("Sending "+converted+" to DMX Channel "+dmx_channel);
-            local.send(dmx_channel,converted);
+            var channel_offset = mapping[j];
+            var dmx_channel = sector * 13 + channel_offset;
+            script.log("Sending "+final_value+" to DMX Channel "+dmx_channel);
+            local.send(dmx_channel,final_value);
         }
 
     };
@@ -194,21 +131,21 @@ function clearDMX() {
 	loadFixtures();
 }
 
-function getParameter(light, channel) {
-	pathName = light.toLowerCase().replace(" ","");
-	var type = fixtures[light].type;
+function getParameter(fixture_name, channel) {
+	pathName = fixture_name.toLowerCase().replace(" ","");
+	var isGroup = groups[fixture_name];
     var parameter;
-	if (type == "group") {
+	if (isGroup) {
 		parameter =  parameterPath.groups[pathName][channel];
 	} else {
 		parameter = parameterPath.lights[pathName][channel];
 	}
     return parameter;
 }
-function setChannelParameter(light_name,channel_name,value){
-    if(light_name!=""){
-        var parameter = getParameter(light_name,channel_name);
-        if(!parameter){script.log("Light "+light_name+" of type "+fixtures[light_name].type+" doesn't have channel "+channel_name); return;}
+function setChannelParameter(fixture_name,channel_name,value){
+    if(fixture_name!=""){
+        var parameter = getParameter(fixture_name,channel_name);
+        if(!parameter){script.log("Light "+fixture_name+" of type "+fixtures[fixture_name].type+" doesn't have channel "+channel_name); return;}
         parameter.set(value);
     }
 }
